5e2f7109d812175790b3a528c9bd605e7bb9f2ab
Farmabarna <a16miqbargim@inspedralbes.cat>
2024-03-05 13:09:58 +0100

    vue funcional TG-4 #closed

--- web/fabianmiquel/src/App.vue
+++ web/fabianmiquel/src/App.vue
@@ -11,47 +11,13 @@
 
 <script>
 export default {
-  data() {
-    return {
-      animales: [],
-      animalesFiltrados: [],
-      filtro: ''
-    };
-  },
-  mounted() {
-    this.obtenerAnimales();
-  },
-  methods: {
-    async obtenerAnimales() {
-      try {
-        const response = await fetch('http://localhost:8000/api/random-animal');
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        this.animales = data;
-        this.animalesFiltrados = data;
-      } catch (error) {
-        console.error("Error al obtener los animales:", error);
-      }
-    },
-    filtrarAnimales() {
-      if (this.filtro === '') {
-        this.animalesFiltrados = this.animales;
-      } else {
-        this.animalesFiltrados = this.animales.filter(animal =>
-          animal.nombre.toLowerCase().includes(this.filtro.toLowerCase()) ||
-          animal.caracteristica.toLowerCase().includes(this.filtro.toLowerCase())
-        );
-      }
-    }
-  }
-};
-</script>
+  data() { return { animales: [], animalesFiltrados: [], filtro: '' }; }, mounted() { this.obtenerAnimales(); }, methods: { async obtenerAnimales() { try { const response = await fetch('http://localhost:8000/api/random-animal'); if (!response.ok) { throw new Error('Network response was not ok'); } const data = await response.json(); this.animales = data; this.animalesFiltrados = data; } catch (error) { console.error("Error al obtener los animales:", error); } }, filtrarAnimales() { if (this.filtro === '') { this.animalesFiltrados = this.animales; } else { this.animalesFiltrados = this.animales.filter(animal => animal.nombre.toLowerCase().includes(this.filtro.toLowerCase()) || animal.caracteristica.toLowerCase().includes(this.filtro.toLowerCase())); } } }
+}; </script>
 
 <style>
 .portada {
   text-align: center;
 }
+
 /* Añade estilos adicionales según tus preferencias */
 </style>
